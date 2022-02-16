8e9c424e5b6542351297739bf17825a1791a0b41
Nishanth <rajkumarnishanth@gmail.com>
2022-02-16 11:53:05 +0530

    Method to add AddressBooks to library

---
+++ AddressBookSystem/AddressBookLibrary.cs
@@ -0,0 +1,28 @@
+﻿namespace AddressBookSystem;
+
+// This class used for keeping collection of AddressBook objects
+internal class AddressBookLibrary
+{
+    private Dictionary<string, AddressBook> library;
+
+    // Default Constructor
+    public AddressBookLibrary()
+    {
+        library = new Dictionary<string, AddressBook>();
+    }
+
+    // Adds new AddressBook to library
+    public void AddAddressBook()
+    {
+        string name = UserInput.GetName("Enter Name of AddressBook: ");
+        if (library.ContainsKey(name) is false && String.IsNullOrEmpty(name) is false)
+        {
+            library.Add(name, new AddressBook());
+            Console.WriteLine("AddressBook Added Successfully");
+        }
+        else if (String.IsNullOrEmpty(name))
+            Console.WriteLine("Invalid name");
+        else
+            Console.WriteLine("AddressBook with that name already exists");
+    }
+}
